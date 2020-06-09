1d83204211b802112beab74dabf88cf5bb5cc935
Anne Fausto <a.r.fausto@gmail.com>
2020-06-09 01:19:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   
   extend Memorable::ClassMethods
-  extend Findable
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
   
